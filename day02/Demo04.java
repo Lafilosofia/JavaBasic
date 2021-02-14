5a505f90338dca095ad67154d0ad1f14c269d9f8
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-02-14 13:08:07 +0800

    Add files via upload

---
+++ day02/Demo04.java
@@ -0,0 +1,16 @@
+package day02;
+/**
+ * 字符串拼接
+ * @author 86180
+ *
+ */
+public class Demo04 {
+public static void main(String[] args){
+	//String字符串 并且使用双引号
+	//String str="这个数字的值"
+	String str = "345";
+	int a = 10;
+	System.out.println(str+10);
+	//字符串拼接 数字+字符串=字符串 字符串+数字=字符串
+}
+}

01f29036b1285e6b69e1d73bfa175d911bf42472
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-02-14 13:02:56 +0800

    Add files via upload

---
+++ day01/Demo01.java
@@ -0,0 +1,21 @@
+package day01;
+/**
+ * 输出一个Hello Word!
+ * @author 86180
+ * 
+ *
+ */
+public class Demo01 {
+	public static void main(String[] args){  //主程序入口
+		//输出语句
+		/*
+		 * print 不换行
+		 * println 换行
+		 */
+		System.out.print("Hello Word!");
+		System.out.println("你好！");
+	}
+	
+	
+
+}
